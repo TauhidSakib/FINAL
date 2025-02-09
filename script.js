d3cc70f2fe328bf3c8a6b8b299fcf08caca80c07
TauhidSakib <106550333+TauhidSakib@users.noreply.github.com>
2025-02-09 18:11:05 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
+    "Are you sure Soha?",
+    "Really sure Soha?",
+    "Are you positive Soha?",
+    "Jaaan please...",
+    "Think about it jaan",
+    "ki holo!",
+    "I'm Sad",
+    "I'm very very very sad...",
     "Ok fine, I will stop asking...",
     "Just kidding, say yes please! ❤️"
 ];
